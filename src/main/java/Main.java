e19e14d2224fc496d8df358325df5259d49b2d11
冯辉斌 <huibin.feng@beantechs.com>
2021-04-22 06:44:05 +0800

    add test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("hello world");
+        String file = null;
+        try {
+            FileOutputStream fileOutputStream = new FileOutputStream(new File(file));
+            fileOutputStream.write(1);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
